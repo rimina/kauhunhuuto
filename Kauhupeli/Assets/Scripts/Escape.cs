c8a9cb3342bbb99b679f36f7653e5371ddb713ef
Frida Lindholm <fridalndhlm@gmail.com>
2021-03-22 19:54:18 +0200

    Päivitys

---
+++ Kauhupeli/Assets/Scripts/Escape.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour {
+
+	
+	void Update () {
+
+		if(Input.GetKeyDown("escape"))
+		{
+			Debug.Log("You escaped");
+			Application.Quit();
+
+		}
+		
+	}
+}
---
+++ Kauhupeli/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomScript : MonoBehaviour {
+
+	public void changescene(string scenename)
+	{
+		SceneManager.LoadScene (scenename);
+	}
+	
+	public void QuitGame ()
+	{
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+}
