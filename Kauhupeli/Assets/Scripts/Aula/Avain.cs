3e7bc86e472fa0e8c0e3573c4f668fea59a4071f
rimina <annaliisa.mattila@gmail.com>
2021-03-29 17:23:06 +0300

    Lääkkeen löytö

--- Kauhupeli/Assets/Scripts/Aula/Avain.cs
+++ Kauhupeli/Assets/Scripts/Aula/Avain.cs
@@ -39,6 +39,4 @@ public class Avain : MonoBehaviour{
             }
         }
     }
-
-
 }
---
+++ Kauhupeli/Assets/Scripts/Kirjasto/Laake.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Laake : MonoBehaviour{
+    public GameObject gameObj;
+
+    private bool laake_ = false;
+    [SerializeField] Puhekupla kupla_;
+
+    void Awake(){
+    	laake_ = GameState.Instance.getLaakeLoydetty();
+        if(laake_){
+            gameObj.SetActive(false);
+        }
+    }
+
+    void OnMouseDown(){
+    	if(!laake_){
+    		laake_ = true;
+    		GameState.Instance.setLaakeLoydetty(laake_);
+            kupla_.show();
+            gameObj.SetActive(false);
+    	}
+    	else{
+    		Debug.Log("You already found the medicine...");
+    	}
+    }
+}
