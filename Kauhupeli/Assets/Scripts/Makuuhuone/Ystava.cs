a00d5a0000ba2433c5147dedc4f1f3cd23fc91fb
rimina <annaliisa.mattila@gmail.com>
2021-03-31 12:17:10 +0300

    Ystävän köhinät

--- Kauhupeli/Assets/Scripts/Makuuhuone/Ystava.cs
+++ Kauhupeli/Assets/Scripts/Makuuhuone/Ystava.cs
@@ -4,18 +4,39 @@ using System.Collections;
 public class Ystava : MonoBehaviour{
 
     [SerializeField] Puhekupla kupla_;
+    [SerializeField] AudioSource sfx_;
+
+    private float timeLeft_ = 2000.0f;
+    private float time_ = 0.0f;
 
     void OnMouseDown(){
         if(GameState.Instance.getLaakeLoydetty()){
-            Debug.Log("You saved your friend!");
             GameState.Instance.setLaakeVietyYstavalle(true);
         }
         else{
-            Debug.Log("Bring me the medicine or I die!");
             if(!kupla_.nakyyko()){
                 kupla_.show();
             }
             else{
+                kupla_.hide();
+            }
+
+            if(!sfx_.isPlaying){
+                sfx_.Play();
+            }
+            else{
+                sfx_.Stop();
+            }
+        }
+    }
+
+    void Update(){
+        float delta = Time.time - time_;
+        if(kupla_.nakyyko()){
+            timeLeft_ -= delta;
+            if(timeLeft_ <= 0.0){
+                timeLeft_ = 2000.0f;
+                time_ = Time.time;
                 kupla_.hide();
             }
         }
